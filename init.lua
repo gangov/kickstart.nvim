3f02d552ae4239713db4c64ae7bc0696813ac7f0
Kaloyan Gangov <gangov1@gmail.com>
2024-06-02 03:44:26 +0800

    signature

--- init.lua
+++ init.lua
@@ -886,6 +886,7 @@ require('lazy').setup({
   require 'kickstart.plugins.maximizer',
   require 'kickstart.plugins.rustaceanvim',
   require 'kickstart.plugins.toggleterm',
+  require 'kickstart.plugins.lsp_signature',
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
