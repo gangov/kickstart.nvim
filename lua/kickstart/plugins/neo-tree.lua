c5cbb752b01522527b68bb20ade0e88a85cb52ec
Kaloyan Gangov <gangov1@gmail.com>
2024-06-02 01:03:11 +0800

    enables tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -14,11 +14,30 @@ return {
     { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
   },
   opts = {
+    close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+    window = {
+      position = 'right',
+      width = 20,
+    },
     filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
         },
+      },
+    },
+    -- Add event handler for setting relative line numbers
+    event_handlers = {
+      {
+        event = 'neo_tree_buffer_enter',
+        handler = function(arg)
+          vim.cmd [[
+              setlocal relativenumber
+            ]]
+        end,
       },
     },
   },
