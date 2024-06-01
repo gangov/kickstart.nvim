f800b6dd9436133d8c779a54500d1aa352279a1e
Kaloyan Gangov <gangov1@gmail.com>
2024-06-02 01:46:14 +0800

    blame, fidget, maximizer

--- init.lua
+++ init.lua
@@ -190,6 +190,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Maximizer keymap
+vim.api.nvim_set_keymap('n', '<Tab><Tab>', '<cmd>lua require("maximizer").toggle()<CR>', { silent = true, noremap = true })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -884,6 +887,9 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.aerial',
+  require 'kickstart.plugins.blame',
+  require 'kickstart.plugins.fidget',
+  require 'kickstart.plugins.maximizer',
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
---
+++ lua/kickstart/plugins/maximizer.lua
@@ -0,0 +1,6 @@
+return {
+  '0x00-ketsu/maximizer.nvim',
+  opts = {
+    -- options
+  },
+}
