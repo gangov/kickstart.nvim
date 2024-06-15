30f293598b9a6fa8466abdca0d543a981dcc614f
Kaloyan Gangov <gangov1@gmail.com>
2024-06-16 01:26:50 +0800

    changes context to 4 lines max

--- lua/kickstart/plugins/nvim-treesitter-context.lua
+++ lua/kickstart/plugins/nvim-treesitter-context.lua
@@ -5,7 +5,7 @@ return {
     require('treesitter-context').setup {
       enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
       throttle = true, -- Throttles plugin updates (may improve performance)
-      max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+      max_lines = 4, -- How many lines the window should span. Values <= 0 mean no limit.
       patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
         default = {
           'class',
