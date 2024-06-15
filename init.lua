b7669a0004b7bfd69e8d3abda8aa7a8a5ee8acc6
Kaloyan Gangov <gangov1@gmail.com>
2024-06-16 01:53:35 +0800

    rust-analyzer optimization

--- init.lua
+++ init.lua
@@ -513,11 +513,11 @@ require('lazy').setup({
           -- code, if the language server you are using supports them
           --
           -- This may be unwanted, since they displace some of your code
-          -- if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-          -- map('<leader>th', function()
-          --   vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-          -- end, '[T]oggle Inlay [H]ints')
-          -- end
+          if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+            map('<leader>th', function()
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+            end, '[T]oggle Inlay [H]ints')
+          end
         end,
       })
 
@@ -537,43 +537,79 @@ require('lazy').setup({
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
-        lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
+      local servers =
+        {
+          -- clangd = {},
+          -- gopls = {},
+          -- pyright = {},
+          rust_analyzer = {
+            rust = {
+              -- Use a separate target dir for Rust Analyzer. Helpful if you want to use Rust
+              -- Analyzer and cargo on the command line at the same time.
+              analyzerTargetDir = 'target/nvim-rust-analyzer',
+            },
+            server = {
+              -- Improve stability
+              extraEnv = {
+                ['CHALK_OVERFLOW_DEPTH'] = '100000000',
+                ['CHALK_SOLVER_MAX_SIZE'] = '100000000',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+            },
+            cargo = {
+              -- Check feature-gated code
+              features = 'all',
+              extraEnv = {
+                -- Skip building WASM, there is never need for it here
+                ['SKIP_WASM_BUILD'] = '1',
+              },
+            },
+            procMacro = {
+              -- Don't expand some problematic proc_macros
+              ignored = {
+                ['async-trait'] = { 'async_trait' },
+                ['napi-derive'] = { 'napi' },
+                ['async-recursion'] = { 'async_recursion' },
+                ['async-std'] = { 'async_std' },
+              },
+            },
+            rustfmt = {
+              -- Use nightly formatting.
+              -- See the polkadot-sdk CI job that checks formatting for the current version used in
+              -- polkadot-sdk.
+              extraArgs = { '+nightly-2024-04-10' },
+            },
+          },
+          -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+          --
+          -- Some languages (like typescript) have entire language plugins that can be useful:
+          --    https://github.com/pmizio/typescript-tools.nvim
+          --
+          -- But for many setups, the LSP (`tsserver`) will work just fine
+          -- tsserver = {},
+          --
+
+          lua_ls = {
+            -- cmd = {...},
+            -- filetypes = { ...},
+            -- capabilities = {},
+            settings = {
+              Lua = {
+                completion = {
+                  callSnippet = 'Replace',
+                },
+                -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+                -- diagnostics = { disable = { 'missing-fields' } },
+              },
             },
           },
         },
-      }
-
-      -- Ensure the servers and tools above are installed
-      --  To check the current status of installed tools and/or manually install
-      --  other tools, you can run
-      --    :Mason
-      --
-      --  You can press `g?` for help in this menu.
-      require('mason').setup()
+        -- Ensure the servers and tools above are installed
+        --  To check the current status of installed tools and/or manually install
+        --  other tools, you can run
+        --    :Mason
+        --
+        --  You can press `g?` for help in this menu.
+        require('mason').setup()
 
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
